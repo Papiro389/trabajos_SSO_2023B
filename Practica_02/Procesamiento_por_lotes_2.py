ab18416f5bdb9c91b4ace9fe935250cb6480c4d7
Papiroflexio <86893740+Papiro389@users.noreply.github.com>
2023-09-12 11:54:03 -0600

    Procesamiento_por_lotes_2.py

--- Practica_02/Procesamiento_por_lotes_2.py
+++ Practica_02/Procesamiento_por_lotes_2.py
@@ -1,62 +1,51 @@
 import os
 import shutil
 import random
-import string
 
-# Función para cambiar letras a dígitos aleatorios
-def letras_a_digitos(texto):
-    resultado = ""
-    for caracter in texto:
-        if caracter.isalpha():
-            resultado += str(random.randint(0, 9))
-        else:
-            resultado += caracter
-    return resultado
+# Función para cambiar letras y dígitos
+def modificar_archivo(ruta_archivo):
+    with open(ruta_archivo, "r") as rt_arch: #read
+        contenido = rt_arch.read()
+        # Reemplazar letras y dígitos
+        nuevos_caracteres = []
+        for c in contenido:
+            if c.isalpha():
+                # Generar dígito aleatorio
+                nuevos_c = random.choice("0123456789")
+                nuevos_caracteres.append(nuevos_c)
+            elif c.isdigit():
+                # Generar letra aleatoria
+                nuevos_c = random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+                nuevos_caracteres.append(nuevos_c)
+            else:
+                nuevos_caracteres.append(c)
+        nuevo_contenido = "".join(nuevos_caracteres)
+    with open(ruta_archivo, "w") as rt_arch: #write
+        rt_arch.write(nuevo_contenido)
 
-# Función para cambiar dígitos a letras aleatorias
-def digitos_a_letras(texto):
-    resultado = ""
-    for caracter in texto:
-        if caracter.isdigit():
-            resultado += random.choice(string.ascii_uppercase)
-        else:
-            resultado += caracter
-    return resultado
+# Función para procesar archivo
+def proceso(ruta_archivo, ruta_carpeta_origen, ruta_carpeta_destino):
 
-# Función para procesar un archivo
-def procesar_archivo(archivo):
-    with open(archivo, 'r') as file:
-        contenido_original = file.read()
+    # Generar una nueva ruta para la copia del archivo
+    ruta_relativa = os.path.relpath(ruta_archivo, ruta_carpeta_origen)
+    nueva_ruta_archivo = os.path.join(ruta_carpeta_destino, ruta_relativa)
 
-    contenido_modificado = letras_a_digitos(contenido_original)
-    contenido_modificado = digitos_a_letras(contenido_modificado)
+    # Crear la carpeta para la copia si no existe
+    nueva_carpeta = os.path.dirname(nueva_ruta_archivo)
+    if not os.path.exists(nueva_carpeta):
+        os.makedirs(nueva_carpeta)
 
-    # Crear una copia del archivo original con un sufijo "_modificado"
-    nombre_archivo, extension = os.path.splitext(archivo)
-    archivo_modificado = nombre_archivo + "_modificado" + extension
+    # Copiar archivo a nueva ruta
+    shutil.copy(ruta_archivo, nueva_ruta_archivo)
 
-    with open(archivo_modificado, 'w') as file:
-        file.write(contenido_modificado)
+    # Modificar archivo copiado
+    modificar_archivo(nueva_ruta_archivo)
 
-# Función para procesar todos los archivos en una carpeta y subcarpetas
-def procesar_carpeta(carpeta):
-    for dirpath, dirnames, filenames in os.walk(carpeta):
-        for filename in filenames:
-            archivo = os.path.join(dirpath, filename)
-            procesar_archivo(archivo)
+    print(f"Archivo procesado: {nueva_ruta_archivo}")
 
-# Ruta de la carpeta que deseas procesar
-carpeta_entrada = "C:/Users/seren/Desktop/Practica_02-main/Sistemas"
-carpeta_salida = "C:/Users/seren/Desktop/Practica_02-main/Sistemas_copy"
-
-# Verifica si la carpeta de salida ya existe y elimínala si es necesario
-if os.path.exists(carpeta_salida):
-    shutil.rmtree(carpeta_salida)
-
-# Copia la carpeta de entrada a la carpeta de salida
-shutil.copytree(carpeta_entrada, carpeta_salida)
-
-# Procesa los archivos en la carpeta de salida
-procesar_carpeta(carpeta_salida)
-
-print("Proceso completado.")
+ruta_carpeta_origen = "C:/Users/seren/Desktop/Practica_02/Sistemas"
+ruta_carpeta_destino = "C:/Users/seren/Desktop/Practica_02/Sistemas_Copia"  # Cambia esto a la ruta de destino deseada
+for raiz, direcciones, archivos in os.walk(ruta_carpeta_origen):
+    for archivo in archivos:
+        ruta_archivo = os.path.join(raiz, archivo)
+        proceso(ruta_archivo, ruta_carpeta_origen, ruta_carpeta_destino)
